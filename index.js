917a6e71a02a878a53e0c863848485bd14270f5a
Jeremiah <jscott4720@gmail.com>
2020-07-30 10:43:40 -0400

    added team search

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ app.get('/home', (req, res) => {
     })
 })
 
-app.get("/home/team/:id", (req, res) =>{
-    Player.find(req.params)
+app.get("/home/player/first_name/:first_name", (req, res) =>{
+    Player.find({first_name: req.params.first_name})
+    .then(player => {
+        res.json(player)
+    })
+})
+
+app.get("/home/teams/:team", (req, res) => {
+    Player.find({"team.name" : req.params.team})
     .then(team => {
         res.json(team)
     })
